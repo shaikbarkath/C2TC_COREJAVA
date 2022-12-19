8019bd60a84d6e12ccddcce2be3f71b1284c617d
barkath <shaikbarkath444@gmail.com>
2022-12-19 22:07:44 +0530

    first commit

--- GoShoppingAppCasestudy1/src/com/tns/framework/ShopFactory.java
+++
@@ -1,6 +0,0 @@
-package com.tns.framework;
-
-public abstract class ShopFactory {
-	public abstract PrimeAcc getNewPrimeAccount(int AccNo, String accNm, Float charges, Boolean isPrime) ;
-	public abstract NormalAcc getNewNormalAccount(int AccNo, String accNm, Float charges);
-	}
